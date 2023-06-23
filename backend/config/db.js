1f99633845427a53379f3068fba1a1fb2f58e86b
Sameer Sawant <sawantsameer666@gmail.com>
2023-06-23 18:05:52 +0530

    Generate JWT Token

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,13 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI);
-        console.log(`MonogDB Connected: ${conn.connection.host}`);
-    } catch (err) {
-        console.error(error.message);
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.error(`Error: ${error.message}`);
         process.exit(1);
     }
-}
+};
 
 export default connectDB;
