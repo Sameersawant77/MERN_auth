92e71572e10469526a28e3a1caaf29bfc9e6df85
Sameer Sawant <sawantsameer666@gmail.com>
2023-06-23 19:42:14 +0530

    Auth Protect MiddlerWare

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { authUser, logoutUser, registerUser, getUserProfile, updateUserProfile } from '../controllers/userController.js';
+import { protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
@@ -9,6 +10,6 @@ router.post('/logout', logoutUser);
 router.post('/', registerUser);
 // router.get('/profile', getUserProfile);
 // router.put('/profile', updateUserProfile);
-router.route('/profile').get(getUserProfile).put(updateUserProfile);
+router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile);
 
 export default router;
